354d296a4c7fa398ec4568b68c56d94124819d68
Michael Patrick Escalambre <kiroval383@gmail.com>
2025-11-18 18:06:18 +0800

    fix: improve formatting and structure in navigation links of personal portfolio

--- personalportfolio/app/page.tsx
+++ personalportfolio/app/page.tsx
@@ -4,20 +4,21 @@ export default function Home() {
   return (
     <div className = "min-h-screen bg-[#d1d1d1]">
       <div className= "mx-auto flex min-h-screen w-full max-w-[1100px] flex-col px-6 lg:px-0" >
-        <header className = "pt-10 pb-6">
+        <header className="pt-10 pb-6">
           <nav>
-            <ul className = "flex justify-center gap-24 text-[1.5rem]">
+            <ul className="flex justify-center gap-24 text-[1.5rem]">
               <li>
                 <Link href="#projects" className="hover:underline">
                   projects
                 </Link>
-                </li>
+              </li>
               <li>
                 <Link href="#home" className="hover:underline">
                   home
                 </Link>
               </li>
-              <Link href="#contacts" className="hover:underline">
+              <li>
+                <Link href="#contacts" className="hover:underline">
                   contacts
                 </Link>
               </li>
